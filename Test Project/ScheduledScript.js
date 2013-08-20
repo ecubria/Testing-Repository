374e00c3e9151a6e7d0d1bbc132795f7dad8692e
Edu.Test <ecubria@tavanoteam.com>
2013-08-20 19:13:40 -0300

    COMMIT COMMIT

---
+++ Test Project/ScheduledScript.js
@@ -0,0 +1,15 @@
+/**
+ * Module Description
+ * 
+ * Version    Date            Author           Remarks
+ * 1.00       20 ago 2013     Eduardo
+ *
+ */
+
+/**
+ * @param {String} type Context Types: scheduled, ondemand, userinterface, aborted, skipped
+ * @returns {Void}
+ */
+function scheduled(type) {
+
+}
